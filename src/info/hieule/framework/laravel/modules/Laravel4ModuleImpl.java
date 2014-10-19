2642adc95e56138fc58fe71bb79a8a1e8f9df6db
trunghieule <letrunghieu.cse09@gmail.com>
2014-10-19 20:52:14 +0700

    Implement getDirectory method

--- src/info/hieule/framework/laravel/modules/Laravel4ModuleImpl.java
+++ src/info/hieule/framework/laravel/modules/Laravel4ModuleImpl.java
@@ -23,6 +23,8 @@
  */
 package info.hieule.framework.laravel.modules;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.netbeans.modules.php.api.phpmodule.PhpModule;
 import org.openide.filesystems.FileObject;
 
@@ -30,7 +32,9 @@ import org.openide.filesystems.FileObject;
  *
  * @author Hieu Le <letrunghieu.cse09@gmail.com>
  */
-public class Laravel4ModuleImpl extends LaravelModuleImpl{
+public class Laravel4ModuleImpl extends LaravelModuleImpl {
+
+    private static final Logger LOGGER = Logger.getLogger(Laravel4ModuleImpl.class.getName());
 
     public Laravel4ModuleImpl(PhpModule phpModule) {
         super(phpModule);
@@ -38,7 +42,105 @@ public class Laravel4ModuleImpl extends LaravelModuleImpl{
 
     @Override
     public FileObject getDirectory(LaravelModule.DIR_TYPE type, LaravelModule.FILE_TYPE fileType, String packageName) {
-        return null;
+        if (packageName != null && packageName.isEmpty()) {
+            packageName = null;
+        }
+        if (type == null && packageName == null) {
+            return null;
+        }
+
+        if (fileType == null && packageName == null) {
+            return getDirectory(type);
+        }
+
+        if (packageName != null) {
+            // TODO package dir
+        }
+
+        FileObject directory = null;
+        StringBuilder sb = new StringBuilder();
+        switch (type) {
+            case APP:
+                if (fileType == null) {
+                    return null;
+                }
+                switch (fileType) {
+                    case COMMANDS:
+                    case CONFIG:
+                    case CONTROLLERS:
+                    case LANG:
+                    case MODELS:
+                    case TESTS:
+                    case VIEWS:
+                        sb.append(fileType.name().toLowerCase());
+                        break;
+                    case MIGRATIONS:
+                    case SEEDS:
+                        sb.append("database/");
+                        sb.append(fileType.name().toLowerCase());
+                        break;
+                    default:
+                        return getDirectory(type);
+
+                }
+                break;
+            case BOOTSTRAP:
+            case PUBLIC:
+                if (fileType == null || fileType == LaravelModule.FILE_TYPE.NONE) {
+                    return getDirectory(type);
+                } else {
+                    return null;
+                }
+            case VENDOR:
+                if (fileType == null) {
+                    return null;
+                }
+                switch (fileType) {
+                    case CONFIG:
+                    case LANG:
+                    case VIEWS:
+                    case MIGRATIONS:
+                        sb.append("src/");
+                        sb.append(fileType.name().toLowerCase());
+                        break;
+                    case TESTS:
+                    case PUBLIC:
+                        sb.append(fileType.name().toLowerCase());
+                        break;
+                    default:
+                        return getDirectory(type);
+
+                }
+                break;
+            default:
+                return null;
+        }
+        directory = getDirectory(type);
+        if (directory == null) {
+            return null;
+        }
+        return directory.getFileObject(sb.toString());
     }
-    
+
+    @Override
+    public FileObject getDirectory(LaravelModule.DIR_TYPE type) {
+        if (type == null) {
+            return null;
+        }
+        FileObject sourceDirectory = getLaravelDirectory();
+        if (sourceDirectory == null) {
+            LOGGER.log(Level.WARNING, "Not found source directory");
+            return null;
+        }
+        String path;
+        switch (type) {
+            case NONE:
+                return null;
+            default:
+                path = type.name().toLowerCase();
+                break;
+        }
+        return sourceDirectory.getFileObject(path);
+    }
+
 }
